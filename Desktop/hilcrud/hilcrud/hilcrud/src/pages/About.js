ba141fb8ea8e7def0dde1bfecee4f5b393b6c208
NathanielTupaz <nathanieltupaz16@gmail.com>
2023-04-29 11:36:41 +0800

    first commit

---
+++ Desktop/hilcrud/hilcrud/hilcrud/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div style={{marginTop: "150px"}}>
+            <h2>
+                {" "}
+                "A course that enlightens every individual capabilities.
+                To strive more and be an independent one. 
+                Associate in Computer Technology gives more of what we are expected, joining this course is such an amazing
+                and a wonderful experience. "
+                    
+            </h2>
+
+        </div>
+    );
+};
+
+export default About;
